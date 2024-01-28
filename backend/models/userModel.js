9f3c0e9eb967110c3a7565abff8efd5514d98953
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-28 21:16:47 +0100

    did the signup operation to check the email format, the strongeness of the password and if the email already used

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -14,5 +16,33 @@ const userSchema = new Schema({
         unique: true
     }
 })
+
+// Static Signup method
+userSchema.statics.signup = async function (email, password) {
+
+    // Validation
+    if (!email || !password) {
+        throw Error('All fiedls must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if(exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
