aa36fb74ff2e3a8b1a8bc0ee29e72e797e28bb58
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-28 03:46:19 +0100

    Now when you add something you see it getting displayed in the displaying part

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 // Components
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
 
     useEffect(() => {
-        fetch('/api/workouts').then(
-            res => res.json()
-        ).then(data => { setWorkouts(data)})
-    }, [])
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+    
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+            console.log(json)
+        };
+        fetchWorkouts();
+    }, [dispatch]);
+    
 
   return (
     <div className='home'>
