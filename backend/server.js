f53865939ac8073cdefd967126cebdda2f2f1779
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-28 20:43:00 +0100

    Putting the besics for authotufication, the user Router, Controller and Model

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workout')
+const userRoutes = require('./routes/user')
 
 
 // Express App
@@ -20,6 +21,7 @@ app.use(express.json())
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
