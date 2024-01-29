134ed3ad9f2cfe69a2b5a61d24b2b741a3fa8a9c
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-29 08:27:19 +0100

    Now the user can enter an email and password in the signup page, and the server will save the user's token in the client's local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // Pages & Components
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <div className='pages'>
           <Routes>
             <Route path='/' element={ <Home /> } />
+            <Route path='/login' element={ <Login /> } />
+            <Route path='/signup' element={ <Signup /> } />
           </Routes>
         </div>
       </BrowserRouter>
