15b2f6daa29cc20ec2b20b60f4907e88bf7fe922
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-30 05:23:22 +0100

    Here we signed each workouts for an exact user in the front end and fixed a problem that is the user gets the previous user's workouts for a second before disappearing, and that is because the workouts don't set to null after every loggout, which we assure to set

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext"
+import { useWorkoutsContext } from "./useWorkoutsContext"
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch: workoutsDispatch } = useWorkoutsContext()
 
     const logout = () => {
         // remove user from storage
@@ -9,6 +11,7 @@ export const useLogout = () => {
 
         // dispatch logout action
         dispatch({type: 'LOGOUT'})
+        workoutsDispatch({ type: 'SET_WORKOUTS', payload: null }) // We do this so when you getting into a page right after loggin out of a different user, you don't see ther previous user's workouts loading for second and disappearing after, so the workouts need to be set to null after every logout.
     }
     return {logout}
 }
