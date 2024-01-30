a6e00c65ac0d0ef0e2fe273f9e70d217b62e790c
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-30 04:59:01 +0100

    Navigate/Redirect the user to Login page if they Logged out or not identified(the token is not exiting), and to the home page if they're already logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 // Pages & Components
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className='pages'>
           <Routes>
-            <Route path='/' element={ <Home /> } />
-            <Route path='/login' element={ <Login /> } />
-            <Route path='/signup' element={ <Signup /> } />
+            <Route path='/' element={ user ? <Home /> : <Navigate to={"/login"} /> } />
+            <Route path='/login' element={ user ? <Navigate to={'/'} /> : <Login /> } />
+            <Route path='/signup' element={ user ? <Navigate to={'/'} /> : <Signup /> } />
           </Routes>
         </div>
       </BrowserRouter>
