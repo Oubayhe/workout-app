15743b9904ff7f6dd0522d47951049a1d2f72b8b
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-01-30 02:57:07 +0100

    Added the authontification in the frontend so the user can't get any data or add or remove any element until he/she is logged in

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,12 +1,21 @@
+import { useAuthContext } from "../hooks/useAuthContext"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 import { formatDistanceToNow } from "date-fns"
 
 const WorkoutDetails = ({ workout }) => {
     const { dispatch } = useWorkoutsContext()
+    const { user } = useAuthContext()
+
+    if (!user) {
+        return 
+    }
 
     const handleClick = async () => {
         const response = await fetch('/api/workouts/' + workout._id, {
-            method: 'DELETE'
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
         })
         const json = await response.json()
         if (response.ok) {
